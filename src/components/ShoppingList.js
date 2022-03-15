1f666b04af1a3312ede07de3c2207684452e775b
Irene <irenemanningm@gmail.com>
2022-03-15 14:52:09 -0500

    Done.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -6,20 +6,39 @@ import Item from "./Item";
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
+  const [searchText, setSearchText] = useState("");
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const itemsToDisplay = items
+    .filter(
+      (item) => selectedCategory === "All" || item.category === selectedCategory
+    )
+    .filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
 
-    return item.category === selectedCategory;
-  });
+    // const newItem = {
+    //   id: uuid(), // the `uuid` library can be used to generate a unique id
+    //   name: itemName,
+    //   category: itemCategory,
+    // };
+    function handleAddItem(e) {
+      e.preventDefault()
+      const name = e.target.elements[0].value
+      console.log(name)
+
+    }
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleAddItem}/>
+      <Filter
+        searchText={searchText}
+        onSearchTextChange={setSearchText}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
